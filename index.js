a7271d4957531c8256c1dfdae64f4dacc4d996dd
Jorge Aguirre <jorge.aguirre.leon@gmail.com>
2016-02-14 16:40:50 +0100

    Initial cool commit

---
+++ index.js
@@ -0,0 +1,14 @@
+(function() {
+  var query = location.search;
+  query = query.substring(1); //Remove initial '?'
+  var params = query.split("&"); //Split into different things
+  var top = "Hola bebé";
+  var bottom = "Que te jodan";
+  for (i=0; i<params.length; i++) {
+    var value = params[i].split("=");
+    if (value[0] === "top") top = decodeURIComponent(value[1]);
+    else if (value[0] === "bottom") bottom = decodeURIComponent(value[1]);
+  }
+  document.getElementById("top-caption").innerText = top;
+  document.getElementById("bottom-caption").innerText = bottom;
+})();
---
+++ route.js
@@ -0,0 +1,27 @@
+document.getElementById('top-caption').addEventListener("keypress", update_url);
+document.getElementById('top-caption').addEventListener("keydown", update_url);
+document.getElementById('top-caption').addEventListener("keyup", update_url);
+
+document.getElementById('bottom-caption').addEventListener("keypress", update_url);
+document.getElementById('bottom-caption').addEventListener("keydown", update_url);
+document.getElementById('bottom-caption').addEventListener("keyup", update_url);
+
+function update_url() {
+  var top = document.getElementById('top-caption');
+  var bottom = document.getElementById('bottom-caption');
+  var top_value = top.value;
+  var bottom_value = bottom.value;
+  if (top_value == "") top_value = "HOLA BEBÉ";
+  if (bottom_value == "") bottom_value = "QUE TE JODAN";
+  var top_value = encode(top_value);
+  var bottom_value = encode(bottom_value);
+
+  var base_url = "http://jorgeaguirreleon.github.io/js-nope";
+  base_url += "?top=" + top_value + "&bottom=" + bottom_value;
+  document.getElementById('result-caption').value = base_url;
+  document.getElementById('result-caption-link').href = base_url;
+}
+
+function encode(text) {
+  return encodeURIComponent(text).replace(/'/g,"%27").replace(/"/g,"%22");
+}
